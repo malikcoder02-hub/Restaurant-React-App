b984d2593ac57203e92df29d4373d1e9e2298a30
Malik Majeed <malikmajeedzia@gmail.com>
2025-08-08 23:41:35 +0500

    till menu section website done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import Homepage from './components/pages/homepage';
+
+
+function App() {
+
+
+  return (
+    <>
+     <h1>Home Page</h1>
+      <Homepage />
+    </>
+  )
+}
+
+export default App
